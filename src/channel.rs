406c1f19d7af20b1651763fe876e630275c3d621
syn-chromatic <synchromatic.github@gmail.com>
2023-09-18 13:41:32 +0300

    update refactor atomic channel

--- src/channel.rs
+++ src/channel.rs
@@ -22,7 +22,7 @@ impl<T> ChannelMessage<T> {
     }
 }
 
-struct ChannelCounters {
+struct ChannelStatus {
     sent_count: Arc<AtomicUsize>,
     sending_count: Arc<AtomicUsize>,
     received_count: Arc<AtomicUsize>,
@@ -30,7 +30,7 @@ struct ChannelCounters {
     concluded_count: Arc<AtomicUsize>,
 }
 
-impl ChannelCounters {
+impl ChannelStatus {
     fn new() -> Self {
         let sent_count: Arc<AtomicUsize> = Arc::new(AtomicUsize::new(0));
         let sending_count: Arc<AtomicUsize> = Arc::new(AtomicUsize::new(0));
@@ -38,7 +38,7 @@ impl ChannelCounters {
         let receiving_count: Arc<AtomicUsize> = Arc::new(AtomicUsize::new(0));
         let concluded_count: Arc<AtomicUsize> = Arc::new(AtomicUsize::new(0));
 
-        ChannelCounters {
+        ChannelStatus {
             sent_count,
             sending_count,
             received_count,
@@ -104,7 +104,7 @@ impl ChannelCounters {
 pub struct AtomicChannel<T> {
     sender: mpsc::Sender<ChannelMessage<T>>,
     receiver: Mutex<mpsc::Receiver<ChannelMessage<T>>>,
-    counters: ChannelCounters,
+    status: ChannelStatus,
 }
 
 impl<T> AtomicChannel<T> {
@@ -115,23 +115,23 @@ impl<T> AtomicChannel<T> {
         ) = mpsc::channel();
 
         let receiver: Mutex<mpsc::Receiver<ChannelMessage<T>>> = Mutex::new(receiver);
-        let counters: ChannelCounters = ChannelCounters::new();
+        let status: ChannelStatus = ChannelStatus::new();
 
         AtomicChannel {
             sender,
             receiver,
-            counters,
+            status,
         }
     }
 
     pub fn send(&self, value: T) -> Result<(), mpsc::SendError<T>> {
-        self.counters.add_sending_count();
+        self.status.add_sending_count();
         let channel_message: ChannelMessage<T> = ChannelMessage::new(value, MessageKind::Value);
         let sent_result: Result<(), mpsc::SendError<ChannelMessage<T>>> =
             self.sender.send(channel_message);
-        self.counters.sub_sending_count();
+        self.status.sub_sending_count();
         if let Ok(_) = sent_result {
-            self.counters.add_sent_count();
+            self.status.add_sent_count();
             return Ok(());
         }
         let error: mpsc::SendError<ChannelMessage<T>> = sent_result.unwrap_err();
@@ -161,37 +161,37 @@ impl<T> AtomicChannel<T> {
     }
 
     pub fn recv(&self) -> Result<(T, MessageKind), mpsc::RecvError> {
-        self.counters.add_receiving_count();
+        self.status.add_receiving_count();
         if let Ok(receiver_guard) = self.receiver.lock() {
             let received_result: Result<ChannelMessage<T>, mpsc::RecvError> = receiver_guard.recv();
             if let Ok(channel_message) = received_result {
                 match channel_message.kind {
-                    MessageKind::Value => self.counters.add_received_count(),
+                    MessageKind::Value => self.status.add_received_count(),
                     MessageKind::Release => {}
                 }
-                self.counters.sub_receiving_count();
+                self.status.sub_receiving_count();
                 return Ok((channel_message.value, channel_message.kind));
             }
         }
-        self.counters.sub_receiving_count();
+        self.status.sub_receiving_count();
         Err(mpsc::RecvError)
     }
 
     pub fn try_recv(&self) -> Result<(T, MessageKind), mpsc::TryRecvError> {
-        self.counters.add_receiving_count();
+        self.status.add_receiving_count();
         if let Ok(receiver_guard) = self.receiver.lock() {
             let received_result: Result<ChannelMessage<T>, mpsc::TryRecvError> =
                 receiver_guard.try_recv();
             if let Ok(channel_message) = received_result {
                 match channel_message.kind {
-                    MessageKind::Value => self.counters.add_received_count(),
+                    MessageKind::Value => self.status.add_received_count(),
                     MessageKind::Release => {}
                 }
-                self.counters.sub_receiving_count();
+                self.status.sub_receiving_count();
                 return Ok((channel_message.value, channel_message.kind));
             }
         }
-        self.counters.sub_receiving_count();
+        self.status.sub_receiving_count();
         Err(mpsc::TryRecvError::Disconnected)
     }
 
@@ -199,26 +199,26 @@ impl<T> AtomicChannel<T> {
         &self,
         timeout: Duration,
     ) -> Result<(T, MessageKind), mpsc::RecvTimeoutError> {
-        self.counters.add_receiving_count();
+        self.status.add_receiving_count();
         if let Ok(receiver_guard) = self.receiver.lock() {
             let received_result: Result<ChannelMessage<T>, mpsc::RecvTimeoutError> =
                 receiver_guard.recv_timeout(timeout);
             if let Ok(channel_message) = received_result {
                 match channel_message.kind {
-                    MessageKind::Value => self.counters.add_received_count(),
+                    MessageKind::Value => self.status.add_received_count(),
                     MessageKind::Release => {}
                 }
-                self.counters.sub_receiving_count();
+                self.status.sub_receiving_count();
                 return Ok((channel_message.value, channel_message.kind));
             }
         }
-        self.counters.sub_receiving_count();
+        self.status.sub_receiving_count();
         Err(mpsc::RecvTimeoutError::Disconnected)
     }
 
     pub fn conclude(&self, kind: MessageKind) {
         match kind {
-            MessageKind::Value => self.counters.add_concluded_count(),
+            MessageKind::Value => self.status.add_concluded_count(),
             MessageKind::Release => {}
         }
     }
@@ -237,28 +237,23 @@ impl<T> AtomicChannel<T> {
     }
 
     pub fn get_sent_count(&self) -> usize {
-        let sent_count: usize = self.counters.get_sent_count();
-        sent_count
+        self.status.get_sent_count()
     }
 
     pub fn get_sending_count(&self) -> usize {
-        let sending_count: usize = self.counters.get_sending_count();
-        sending_count
+        self.status.get_sending_count()
     }
 
     pub fn get_received_count(&self) -> usize {
-        let received_count: usize = self.counters.get_received_count();
-        received_count
+        self.status.get_received_count()
     }
 
     pub fn get_receiving_count(&self) -> usize {
-        let receiving_count: usize = self.counters.get_receiving_count();
-        receiving_count
+        self.status.get_receiving_count()
     }
 
     pub fn get_concluded_count(&self) -> usize {
-        let concluded_count: usize = self.counters.get_concluded_count();
-        concluded_count
+        self.status.get_concluded_count()
     }
 
     pub fn clear_receiver(&self) {
