e4855152c0f14db526a13dad01851bcff0019b31
syn-chromatic <synchromatic.github@gmail.com>
2024-02-05 09:43:47 +0200

    add more tests

--- tests/resize_test.rs
+++ tests/resize_test.rs
@@ -1,70 +1,79 @@
-use std::thread::sleep;
-use std::time::Duration;
+mod resize_test {
+    use std::thread::sleep;
+    use std::time::Duration;
 
-use thread_manager::ThreadManager;
+    use thread_manager::ThreadManager;
 
-#[test]
-fn resize_test() {
-    let mut thread_manager = ThreadManager::<()>::new(4);
+    #[test]
+    fn resize_test() {
+        let mut thread_manager = ThreadManager::<()>::new(4);
 
-    for _ in 0..100 {
-        thread_manager.execute(|| {
-            sleep(Duration::from_millis(10));
-        });
+        for _ in 0..100 {
+            thread_manager.execute(|| {
+                sleep(Duration::from_millis(10));
+            });
+        }
+
+        thread_manager.resize(12);
+
+        for _ in 0..100 {
+            thread_manager.execute(|| {
+                sleep(Duration::from_millis(10));
+            });
+        }
+
+        thread_manager.resize(6);
+
+        for _ in 0..100 {
+            thread_manager.execute(|| {
+                sleep(Duration::from_millis(10));
+            });
+        }
+
+        thread_manager.join();
+
+        let job_dist: Vec<usize> = thread_manager.job_distribution();
+        let dist_sum: usize = job_dist.iter().sum();
+        println!("Distribution: {:?} | Sum: {}", job_dist, dist_sum);
     }
-
-    thread_manager.resize(12);
-
-    for _ in 0..100 {
-        thread_manager.execute(|| {
-            sleep(Duration::from_millis(10));
-        });
-    }
-
-    thread_manager.resize(6);
-
-    for _ in 0..100 {
-        thread_manager.execute(|| {
-            sleep(Duration::from_millis(10));
-        });
-    }
-
-    thread_manager.join();
-
-    let job_dist: Vec<usize> = thread_manager.job_distribution();
-    let dist_sum: usize = job_dist.iter().sum();
-    println!("Distribution: {:?} | Sum: {}", job_dist, dist_sum);
 }
 
-#[test]
-fn asymmetric_resize_test() {
-    let mut thread_manager = ThreadManager::<()>::new_asymmetric(4, 2);
+mod asymmetric_resize_test {
+    use std::thread::sleep;
+    use std::time::Duration;
 
-    for _ in 0..100 {
-        thread_manager.execute(|| {
-            sleep(Duration::from_millis(10));
-        });
+    use thread_manager::ThreadManager;
+
+    #[test]
+    fn asymmetric_resize_test() {
+        let mut thread_manager = ThreadManager::<()>::new_asymmetric(4, 2);
+
+        for _ in 0..100 {
+            thread_manager.execute(|| {
+                sleep(Duration::from_millis(10));
+            });
+        }
+
+        thread_manager.resize(12);
+
+        for _ in 0..100 {
+            thread_manager.execute(|| {
+                sleep(Duration::from_millis(10));
+            });
+        }
+
+        thread_manager.resize(6);
+
+        for _ in 0..100 {
+            thread_manager.execute(|| {
+                sleep(Duration::from_millis(10));
+            });
+        }
+
+        thread_manager.join();
+
+        let job_dist: Vec<usize> = thread_manager.job_distribution();
+        let dist_sum: usize = job_dist.iter().sum();
+        println!("Distribution: {:?} | Sum: {}", job_dist, dist_sum);
     }
-
-    thread_manager.resize(12);
-
-    for _ in 0..100 {
-        thread_manager.execute(|| {
-            sleep(Duration::from_millis(10));
-        });
-    }
-
-    thread_manager.resize(6);
-
-    for _ in 0..100 {
-        thread_manager.execute(|| {
-            sleep(Duration::from_millis(10));
-        });
-    }
-
-    thread_manager.join();
-
-    let job_dist: Vec<usize> = thread_manager.job_distribution();
-    let dist_sum: usize = job_dist.iter().sum();
-    println!("Distribution: {:?} | Sum: {}", job_dist, dist_sum);
 }
